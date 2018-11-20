dbb7870a89a0b3118f83dc3e414aff25dd0b6f44
GOKUL G.K <talktothewhitehat@gmail.com>
2018-11-20 21:28:28 +0530

    Update Project_Model.py

--- Project_Model.py
+++ Project_Model.py
@@ -1,22 +1,25 @@
 #### Imporing Packages
 
+import random
+import pickle
+
 from nltk.classify import ClassifierI
+
 from nltk.tokenize import word_tokenize
 
 from statistics import mode
 
-import random
-import pickle
+
 
 ### vote class for running Algorithms and determining the confidence
 
 class Vote_(ClassifierI):
-    def __init__(self, *classifier):
-        self._classifier = classifier
+    def __init__(self, *classifier_):
+        self._classifier_ = classifier_
 
     def classify(self, feature):
         vote = []
-        for c in self._classifier:
+        for c in self._classifier_:
             v = c.classify(feature)
             vote.append(v)
         return mode(vote)
@@ -32,6 +35,7 @@ class Vote_(ClassifierI):
         return confidence_
 
 
+### Loading Saved Featuers
 document = open("pickled_algorithms/document.pickle", "rb")
 documents = pickle.load(document)
 document.close()
@@ -41,7 +45,7 @@ word_features = pickle.load(word_feature)
 word_feature.close()
 
 
-def find_feature(document):
+def Find_feature(document):
     word = word_tokenize(document)
     feature = {}
     for w in word_features:
@@ -49,36 +53,38 @@ def find_feature(document):
 
     return feature
 
-featureset = [(find_feature(rev), category) for (rev, category) in documents]
+featureset = [(Find_feature(r), category) for (r, category) in documents]
 
 random.shuffle(featureset)
 testing_set = featureset[1400:]
 training_set = featureset[:1433]
 
-open_file = open("pickled_algorithms/LinearSVC_classifier.pickle", "rb")
-LinearSVC = pickle.load(open_file)
-open_file.close()
+### Loading Saved Classifiers
 
-open_file = open("pickled_algorithms/SGDC_classifier.pickle", "rb")
-SGDC = pickle.load(open_file)
-open_file.close()
+open_file_LSV = open("pickled_algorithms/LinearSVC_classifier.pickle", "rb")
+LinearSVC = pickle.load(open_file_LSV)
+open_file_LSV.close()
+
+open_file_SGDC = open("pickled_algorithms/SGDC_classifier.pickle", "rb")
+SGDC = pickle.load(open_file_SGDC)
+open_file_SGDC.close()
 
 
-open_file_ = open("pickled_algorithms/naivebayes.pickle", "rb")
-naive_bayes = pickle.load(open_file_)
-open_file_.close()
+open_file_NB = open("pickled_algorithms/naivebayes.pickle", "rb")
+naive_bayes = pickle.load(open_file_NB)
+open_file_NB.close()
 
-open_file = open("pickled_algorithms/MNB_classifier.pickle", "rb")
-MNB = pickle.load(open_file)
-open_file.close()
+open_file_MNB = open("pickled_algorithms/MNB_classifier.pickle", "rb")
+MNB = pickle.load(open_file_MNB)
+open_file_MNB.close()
 
-open_file = open("pickled_algorithms/BernoulliNB_classifier.pickle", "rb")
-BernoulliNB= pickle.load(open_file)
-open_file.close()
+open_file_BNB = open("pickled_algorithms/BernoulliNB_classifier.pickle", "rb")
+BernoulliNB= pickle.load(open_file_BNB)
+open_file_BNB.close()
 
-open_file = open("pickled_algorithms/LogisticRegression_classifier.pickle", "rb")
-LogisticRegression = pickle.load(open_file)
-open_file.close()
+open_file_LR = open("pickled_algorithms/LogisticRegression_classifier.pickle", "rb")
+LogisticRegression = pickle.load(open_file_LR)
+open_file_LR.close()
 
 # Making Instance of the class vote
 Classify = Vote_(
@@ -86,5 +92,5 @@ Classify = Vote_(
             )
 
 def Analayse(text):
-    feat = find_feature(text)
+    feat = Find_feature(text)
     return Classify.classify(feat)
