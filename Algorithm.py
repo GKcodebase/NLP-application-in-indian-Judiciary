a3561c7d721676cb2bfc2675fcb0d15a12cae764
GOKUL G.K <talktothewhitehat@gmail.com>
2018-11-20 21:26:10 +0530

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -2,11 +2,13 @@
 ###### Import packages
 import random
 import nltk
-from  nltk.stem import  WordNetLemmatizer
 from nltk.classify.scikitlearn import SklearnClassifier
+
+from  nltk.stem import  WordNetLemmatizer
+
 from nltk.tokenize import word_tokenize
 
-#### DAtasets: Laws in indian cyber laws of IT ACT
+#### DAtasets
 law1 = open("Laws/ATTRIBUTION, ACKNOWLEDGMENT AND DESPATCH OF ELECTRONIC RECORDS.txt", "r").read()
 law2 = open("Laws/Digital Signature.txt", "r").read()
 law3 = open("Laws/DUTIES OF SUBSCRIBERS.txt", "r").read()
@@ -33,184 +35,192 @@ for p in law1.split('\n'):
     ### Making a tuple out of splited line
     document.append((p, "law_1"))
     ### Tokenizing the tagged line
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         ### lemmatizing each tokenized words
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
         #### removing stopwords
-    law_1 = nltk.pos_tag(words)
-    for w_ in law_1:
-        if w_[1][0] in allowed_word_type:
-            all_word.append(w_[0].lower())
+    law_1 = nltk.pos_tag(word)
+    for i in law_1:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ### tagging all words in law2 as law_2
 
 for p in law2.split('\n'):
     document.append((p, "law_2"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_2 = nltk.pos_tag(words)
-    for w in law_2:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_2 = nltk.pos_tag(word)
+    for i in law_2:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
 ### tagging all words in law3 as law_3
 for p in law3.split('\n'):
     document.append((p, "law_3"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_3 = nltk.pos_tag(words)
-    for w in law_3:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_3 = nltk.pos_tag(word)
+    for i in law_3:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 ###########################################
 # ### tagging all words in law4 as law_4
 for p in law4.split('\n'):
     document.append((p, "law_4"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_4 = nltk.pos_tag(words)
-    for w in law_4:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_4 = nltk.pos_tag(word)
+    for i in law_4:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
 ### tagging all words in law5 as law_5
 for p in law5.split('\n'):
     document.append((p, "law_5"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_5 = nltk.pos_tag(words)
-    for w in law_5:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_5 = nltk.pos_tag(word)
+    for i in law_5:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
 ### tagging all words in law5 as law_5
 for p in law6.split('\n'):
     document.append((p, "law_6"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_6 = nltk.pos_tag(words)
-    for w in law_6:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_6 = nltk.pos_tag(word)
+    for i in law_6:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
 ### tagging all words in law7 as law_7
 for p in law7.split('\n'):
     document.append((p, "law_7"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_7 = nltk.pos_tag(words)
-    for w in law_7:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_7 = nltk.pos_tag(word)
+    for i in law_7:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law8 as law_8
+
 
 for p in law8.split('\n'):
     document.append((p, "law_8"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_8 = nltk.pos_tag(words)
-    for w in law_8:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_8 = nltk.pos_tag(word)
+    for i in law_8:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law9 as law_9
 
 for p in law9.split('\n'):
     document.append((p, "law_9"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_9 = nltk.pos_tag(words)
-    for w in law_9:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_9 = nltk.pos_tag(word)
+    for i in law_9:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law10 as law_10
 
 for p in law10.split('\n'):
     document.append((p, "law_10"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_10 = nltk.pos_tag(words)
-    for w in law_10:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_10 = nltk.pos_tag(word)
+    for i in law_10:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law11 as law_11
 
 for p in law11.split('\n'):
     document.append((p, "law_11"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_11 = nltk.pos_tag(words)
-    for w in law_11:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_11 = nltk.pos_tag(word)
+    for i in law_11:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law12 as law_12
 
 for p in law12.split('\n'):
     document.append((p, "law_12"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_12 = nltk.pos_tag(words)
-    for w in law_12:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_12 = nltk.pos_tag(word)
+    for i in law_12:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law13 as law_13
 
 for p in law13.split('\n'):
     document.append((p, "law_13"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_13 = nltk.pos_tag(words)
-    for w in law_13:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_13 = nltk.pos_tag(word)
+    for i in law_13:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ###########################################
+### tagging all words in law14 as law_14
 
 for p in law14.split('\n'):
     document.append((p, "law_14"))
-    words = word_tokenize(p)
-    for i in words:
+    word = word_tokenize(p)
+    for i in word:
         lemmatizer = WordNetLemmatizer()
         lemmatizer.lemmatize(i)
-    law_14 = nltk.pos_tag(words)
-    for w in law_14:
-        if w[1][0] in allowed_word_type:
-            all_word.append(w[0].lower())
+    law_14 = nltk.pos_tag(word)
+    for i in law_14:
+        if i[1][0] in allowed_word_type:
+            all_word.append(i[0].lower())
 
 ### every tagged word are tagged in document and pickled
 import pickle
@@ -219,26 +229,26 @@ save_document = open("pickled_algorithms/document.pickle", "wb")
 pickle.dump(document, save_document)
 save_document.close()
 #### Frequency distribution of tokenized words
-all_words = nltk.FreqDist(all_word)
+all_word = nltk.FreqDist(all_word)
 
 #### Selecting most frequent words as feature set
-word_feature = list(all_words.keys())[:3000]
-save_word_features = open("pickled_algorithms/word_features.pickle", "wb")
-pickle.dump(word_feature, save_word_features)
-save_word_features.close()
+word_feature = list(all_word.keys())[:3000]
+save_word_feature = open("pickled_algorithms/word_features.pickle", "wb")
+pickle.dump(word_feature, save_word_feature)
+save_word_feature.close()
 
 
-def find_feature(document):
-    words = word_tokenize(document)
+def Find_feature(document):
+    word = word_tokenize(document)
 
     feature = {}
     for w in word_feature:
-        feature[w] = (w in words)
+        feature[w] = (w in word)
 
     return feature
 
 
-featureset = [(find_feature(r), category) for (r, category) in document]
+featureset = [(Find_feature(r), category) for (r, category) in document]
 
 random.shuffle(featureset)
 
@@ -257,15 +267,17 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 
 from sklearn.svm import  LinearSVC
 
-
+############## Stochatic Gradient Descent
 SGDC = SklearnClassifier(SGDClassifier())
 SGDC.train(training_set)
 print("SGD Classifier accuracy percentage:", nltk.classify.accuracy(SGDC, testing_set) * 100)
+
 
 save_classifier = open("pickled_algorithms/SGDC_classifier.pickle", "wb")
 pickle.dump(SGDC, save_classifier)
 save_classifier.close()
 
+######## Naive Bayes
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 print(" Naive Bayes  accuracy percentage:", (nltk.classify.accuracy(classifier, testing_set)) * 100)
 
@@ -274,6 +286,7 @@ save_classifier = open("pickled_algorithms/naivebayes.pickle", "wb")
 pickle.dump(classifier, save_classifier)
 save_classifier.close()
 
+## linear SVM
 LinearSVC = SklearnClassifier(LinearSVC())
 LinearSVC.train(training_set)
 print("LinearSVC  accuracy percentage:", (nltk.classify.accuracy(LinearSVC, testing_set)) * 100)
@@ -282,6 +295,7 @@ save_classifier = open("pickled_algorithms/LinearSVC_classifier.pickle", "wb")
 pickle.dump(LinearSVC, save_classifier)
 save_classifier.close()
 
+### Multinomial Naive Bayes
 MNB = SklearnClassifier(MultinomialNB())
 MNB.train(training_set)
 print("Multinomial  naive bayes accuracy percentage:", (nltk.classify.accuracy(MNB, testing_set)) * 100)
@@ -290,6 +304,7 @@ save_classifier = open("pickled_algorithms/MNB_classifier.pickle", "wb")
 pickle.dump(MNB, save_classifier)
 save_classifier.close()
 
+### Bernoulli naive Bayes
 BernoulliNB = SklearnClassifier(BernoulliNB())
 BernoulliNB.train(training_set)
 print("Bernoulli naive bayes accuracy percentage:", (nltk.classify.accuracy(BernoulliNB, testing_set)) * 100)
@@ -298,10 +313,10 @@ save_classifier = open("pickled_algorithms/BernoulliNB_classifier.pickle", "wb")
 pickle.dump(BernoulliNB, save_classifier)
 save_classifier.close()
 
+### Logistic Regression
 LogisticRegression = SklearnClassifier(LogisticRegression())
 LogisticRegression.train(training_set)
-print("LogisticRegression  accuracy percentage:",
-      (nltk.classify.accuracy(LogisticRegression, testing_set)) * 100)
+print("LogisticRegression  accuracy percentage:",      (nltk.classify.accuracy(LogisticRegression, testing_set)) * 100)
 
 save_classifier = open("pickled_algorithms/LogisticRegression_classifier.pickle", "wb")
 pickle.dump(LogisticRegression, save_classifier)
